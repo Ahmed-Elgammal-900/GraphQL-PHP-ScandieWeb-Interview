9cb8dc28ce799551b3b419d68eb7cc80fe79b9b5
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2024-11-28 02:55:46 +0200

    Api Dpendencies

---
+++ api/Types/ItemType.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Types;
+
+use GraphQL\Type\Definition\{ObjectType, Type};
+
+final class ItemType extends ObjectType
+{
+    public function __construct()
+    {
+        $config = [
+            'name' => 'Attribute',
+            'fields' => [
+                'displayValue' => Type::string(),
+                'value' => Type::string(),
+                'id' => Type::string()
+            ]
+        ];
+
+        parent::__construct($config);
+    }
+}
---
+++ api/Utils/Container.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Utils;
+
+use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Laminas\Diactoros\ResponseFactory;
+use Psr\Http\Message\ResponseInterface;
+use Api\Types\{
+    ItemType, 
+    CategoryType, 
+    CurrencyType, 
+    AttributesType, 
+    PriceType, 
+    ProductType, 
+    InputType, 
+    QueryType, 
+    MutationType
+};
+
+final class Container
+{
+    public static function createContainer(): ContainerInterface
+    {
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->addDefinitions([
+            
+            LoggerInterface::class => function () {
+                $logger = new Logger('graphql');
+                $logger->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log'));
+                return $logger;
+            },
+
+            ResponseInterface::class => function (): ResponseInterface {
+                return (new ResponseFactory())->createResponse();
+            },
+
+            ItemType::class => function (): ItemType {
+                return new ItemType();
+            },
+
+            CategoryType::class => function (): CategoryType {
+                return new CategoryType();
+            },
+
+            CurrencyType::class => function (): CurrencyType {
+                return new CurrencyType();
+            },
+
+            AttributesType::class => function (ContainerInterface $container): AttributesType {
+                return new AttributesType($container->get(ItemType::class));
+            },
+
+            PriceType::class => function (ContainerInterface $container): PriceType {
+                return new PriceType($container->get(CurrencyType::class));
+            },
+
+            ProductType::class => function (ContainerInterface $container): ProductType {
+                return new ProductType($container->get(AttributesType::class), $container->get(PriceType::class));
+            },
+
+            InputType::class => function (): InputType {
+                return new InputType();
+            },
+
+            QueryType::class => function (ContainerInterface $container): QueryType {
+                return new QueryType($container->get(CategoryType::class), $container->get(ProductType::class));
+            },
+
+            MutationType::class => function (ContainerInterface $container): MutationType {
+                return new MutationType($container->get(InputType::class));
+            },
+        ]);
+
+        return $containerBuilder->build();
+    }
+}
