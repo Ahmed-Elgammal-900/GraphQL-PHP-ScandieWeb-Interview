9c3d76f9d98583587eff04dccce436f5521f332b
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2024-11-29 15:57:42 +0200

    logs erorr

--- api/index.php
+++ api/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Api\Utils\{GraphQLHandle, Container};
 
 ini_set('display_errors', 1);
+ini_set('log_errors', 1); 
 error_reporting(E_ALL);
 
 $container = Container::createContainer();
