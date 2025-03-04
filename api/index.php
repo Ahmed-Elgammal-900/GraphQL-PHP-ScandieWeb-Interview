fc0c966af866aa4cee2568b8fba2199c59d9f4d5
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2025-03-04 02:52:17 +0200

    add routing

--- api/index.php
+++ api/index.php
@@ -4,20 +4,46 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Api\Utils\{GraphQLHandler, Container};
 use GuzzleHttp\Psr7\ServerRequest;
-
-$container = Container::createContainer();
+use FastRoute\{RouteCollector};
 
 $request = ServerRequest::fromGlobals();
+$httpMethod = $request->getMethod();
+$uri = $request->getUri()->getPath();
 
-$handler = new GraphQLHandler($container);
 
-$response = $handler->handle($request);
+$dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r): void {
+    $r->post('/graphql', [GraphQLHandler::class, 'handle']);
+});
 
-http_response_code($response->getStatusCode());
-foreach ($response->getHeaders() as $name => $values) {
-    foreach ($values as $value) {
-        header(sprintf('%s: %s', $name, $value), false);
-    }
+$routeInfo = $dispatcher->dispatch(
+    $httpMethod,
+    $uri
+);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        http_response_code(404);
+        echo json_encode(['error' => 'Not Found']);
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        http_response_code(405);
+        echo json_encode(['error' => 'Method Not Allowed', 'allowed' => $allowedMethods]);
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        [$class, $method] = $routeInfo[1];
+
+        $container = Container::createContainer();
+        $graphQL = new $class($container);
+        $response = $graphQL->$method($request);
+
+        http_response_code($response->getStatusCode());
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header(sprintf('%s: %s', $name, $value), false);
+            }
+        }
+
+        echo $response->getBody();
+        break;
 }
-
-echo $response->getBody();
